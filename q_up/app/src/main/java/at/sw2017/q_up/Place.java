c89635e3633649a2a5146f469801959adaaca3e5
tinagojak <tina.gojak@live.com>
2017-03-29 09:33:13 +0300

    [TG, PS] Created User and Place classes

---
+++ q_up/app/src/main/java/at/sw2017/q_up/Place.java
@@ -0,0 +1,14 @@
+package at.sw2017.q_up;
+
+/**
+ * Created by tinag on 29.3.2017..
+ */
+
+public class Place {
+    private Integer placeId;
+    private String placeName;
+    private Float latitude;
+    private Float longitude;
+    private Float avgRating;
+    private Integer avgProcessingSecs;
+}
---
+++ q_up/app/src/main/java/at/sw2017/q_up/User.java
@@ -0,0 +1,12 @@
+package at.sw2017.q_up;
+
+/**
+ * Created by tinag on 29.3.2017..
+ */
+
+public class User {
+    private Integer userId;
+    private String userName;
+    private String password;
+    private Integer idCheckInPlace;
+}
