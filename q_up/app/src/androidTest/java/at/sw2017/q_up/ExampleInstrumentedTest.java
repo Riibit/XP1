730cbe4096e6ca470051dd9fb8cdb32e30eb039a
RolandBarta <rbarta@student.tugraz.at>
2017-03-29 10:35:45 +0200

    UI test Button written

--- q_up/app/src/androidTest/java/at/sw2017/q_up/ExampleInstrumentedTest.java
+++ q_up/app/src/androidTest/java/at/sw2017/q_up/ExampleInstrumentedTest.java
@@ -2,11 +2,16 @@ package at.sw2017.q_up;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,6 +21,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +32,15 @@ public class ExampleInstrumentedTest {
 
         assertEquals("at.sw2017.q_up", appContext.getPackageName());
     }
+
+
+    @Test
+    public void testLoginButton() throws Exception {
+      onView(withText("login")).perform(click());
+
+
+
+    }
 }
+
+
