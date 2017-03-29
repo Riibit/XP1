a0593192d5ae26fc5efcf6564d7340fb5010ce0b
fanconng <esadriu@tugraz.at>
2017-03-29 11:19:24 +0200

    [ES, MD] changed default coordinates to "Graz" in maps

--- Agmapstest/app/src/main/java/at/sw2017/agmapstest/MapsActivity.java
+++ Agmapstest/app/src/main/java/at/sw2017/agmapstest/MapsActivity.java
@@ -59,8 +59,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng graz = new LatLng(47, 15);
+        mMap.addMarker(new MarkerOptions().position(graz).title("Marker in Graz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(graz));
     }
 }
