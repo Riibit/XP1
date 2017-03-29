72508b5c2a1960e3ba749db5c1c776f327b2b0e8
mirzonitr1 <tica@student.tugraz.at>
2017-03-29 14:38:45 +0200

    [MT MD] Google MAps RECAPs

--- q_up/app/src/main/java/at/sw2017/q_up/MainActivity.java
+++ q_up/app/src/main/java/at/sw2017/q_up/MainActivity.java
@@ -1,7 +1,10 @@
 package at.sw2017.q_up;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnSwitchScreens = (Button) findViewById(R.id.btnSwitchScreens);
 
+        btnSwitchScreens.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                switchActivities();
+            }
+        });
+    }
+
+    public void switchActivities() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
