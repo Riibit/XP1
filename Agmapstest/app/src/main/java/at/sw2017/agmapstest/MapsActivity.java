5ab9f28750b40e6bb800db9c2f5b47d9a025a318
fanconng <esadriu@tugraz.at>
2017-03-29 11:01:34 +0200

    [ES,MD] Map button functionality

--- Agmapstest/app/src/main/java/at/sw2017/agmapstest/MapsActivity.java
+++ Agmapstest/app/src/main/java/at/sw2017/agmapstest/MapsActivity.java
@@ -1,7 +1,10 @@
 package at.sw2017.agmapstest;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,6 +17,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    public void mapsGoBack() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +29,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button buttonMapsBack = (Button) findViewById(R.id.buttonMapsBack);
+
+        buttonMapsBack.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                mapsGoBack();
+            }
+
+        });
+
+
     }
+
 
 
     /**
