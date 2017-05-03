f94a883cc284d962d7e286c683129435b3a1403a
tinagojak <tina.gojak@live.com>
2017-05-03 16:58:07 +0200

    [ES, TG] Click on Place in List to go to Place details

--- q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
+++ q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
@@ -1,8 +1,11 @@
 package at.sw2017.q_up;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -21,16 +24,34 @@ public class PlaceViewList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_places);
 
-        List<String> places_to_show = new ArrayList<String>();
+        final List<String> places_to_show = new ArrayList<String>();
+        final List<String> place_ids = new ArrayList<String>();
         DatabaseHandler db_handle = QUpApp.getInstance().getDBHandler();
 
         // fill list with places from DB
         for (Place p : db_handle.getPlacesList()) {
             places_to_show.add(p.placeName);
+            place_ids.add(p.placeId);
         }
 
         ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places_to_show);
         ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(myAdapter);
+
+        list.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String p = place_ids.get(position);
+                goToPlace(p);
+            }
+        });
+    }
+
+    public void goToPlace(String id) {
+
+        Intent intent = new Intent(PlaceViewList.this, PlaceDetails.class);
+        intent.putExtra("id", id);
+        startActivity(intent);
+
     }
 }
