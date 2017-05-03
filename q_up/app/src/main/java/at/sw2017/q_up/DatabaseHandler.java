3fd25972c6312d0a76026f1f18f9d04c0ae9a016
Peter Sammer <sammer.p@gmail.com>
2017-05-03 10:02:47 +0200

    [PS, AF] show places from database in places list

--- q_up/app/src/main/java/at/sw2017/q_up/DatabaseHandler.java
+++ q_up/app/src/main/java/at/sw2017/q_up/DatabaseHandler.java
@@ -20,11 +20,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
--- q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
+++ q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
@@ -7,23 +7,30 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Created by fanconng on 05/04/2017.
  */
 
 public class PlaceViewList extends Activity {
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_list_places);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_places);
 
-      String[] stores = new String[]{"McDonalds", "Davinci", "Hofer", "Billa"};
-      ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stores);
-      ListView list = (ListView) findViewById(R.id.list);
-      list.setAdapter(myAdapter);
+        List<String> places_to_show = new ArrayList<String>();
+        DatabaseHandler db_handle = QUpApp.getInstance().getDBHandler();
 
+        // fill list with places from DB
+        for (Place p : db_handle.getPlacesList()) {
+            places_to_show.add(p.placeName);
+        }
 
-   }
-
+        ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places_to_show);
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(myAdapter);
+    }
 }
