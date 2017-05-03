b157fb620219b845356ceaa4a925a9bf47956368
Alex Frewein <alex5.frewein@gmail.com>
2017-05-03 16:43:36 +0200

    [ES,TG] Place List View Click Listener

--- q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
+++ q_up/app/src/main/java/at/sw2017/q_up/PlaceViewList.java
@@ -1,11 +1,14 @@
 package at.sw2017.q_up;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -14,7 +17,14 @@ import java.util.ArrayList;
  * Created by fanconng on 05/04/2017.
  */
 
-public class PlaceViewList extends Activity {
+public class PlaceViewList extends Activity{
+
+
+    public void mapsGoDetails(String id) {
+        Intent intent2 = new Intent(PlaceViewList.this, PlaceDetails.class);
+        intent2.putExtra("id", id);
+        startActivity(intent2);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +39,24 @@ public class PlaceViewList extends Activity {
             places_to_show.add(p.placeName);
         }
 
+
+
         ListAdapter myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, places_to_show);
         ListView list = (ListView) findViewById(R.id.list);
         list.setAdapter(myAdapter);
+
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String str_id = id +"";
+                mapsGoDetails(str_id);
+
+            }
+        });
+
+
     }
+
+
 }
