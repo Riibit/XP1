d02cc3c3bd9cf65c37fca6d0f5f24c43a2ac77e6
tinagojak <tina.gojak@live.com>
2017-05-10 11:32:29 +0200

    [AP, TG] Clear the form text after registration

--- q_up/app/src/main/java/at/sw2017/q_up/ProfileActivity.java
+++ q_up/app/src/main/java/at/sw2017/q_up/ProfileActivity.java
@@ -23,7 +23,7 @@ public class ProfileActivity extends AppCompatActivity {
         usernameText.setText(currentUser.userName);
 
         db_handle = QUpApp.getInstance().getDBHandler();
-        Place place = new Place();
+        Place place;
         for (Place p : db_handle.getPlacesList()) {
             if (p.placeId.equals(currentUser.idCheckInPlace)) {
                 place = p;
