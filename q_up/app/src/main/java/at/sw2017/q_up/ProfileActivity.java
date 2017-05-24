59bafccda46f9f7a26ccca93c5d9d3f5a527a82a
tinagojak <tina.gojak@live.com>
2017-05-24 14:16:07 +0200

    [TG, AP] Made Login and Registration screen scrollable

--- q_up/app/src/main/java/at/sw2017/q_up/ProfileActivity.java
+++ q_up/app/src/main/java/at/sw2017/q_up/ProfileActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +16,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_profile);
 
         TextView usernameText = (TextView) findViewById(R.id.textView);
