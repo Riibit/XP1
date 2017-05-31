6c6ef9780886b1683eb029bc8bb00d37aaeb5cf0
tina <tina.gojak@live.com>
2017-05-31 16:29:20 +0200

    [TG, AP] Edited User Profile layout, made Layouts for password and user info change

---
+++ q_up/app/src/main/java/at/sw2017/q_up/ChangePasswordActivity.java
@@ -0,0 +1,17 @@
+package at.sw2017.q_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+
+
+        //textView.setText : Changing password for user + username
+    }
+}
